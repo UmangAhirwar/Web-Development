0dd64b83add1b41ee463c272a46ad6da4b0d15ae
Umang Ahirwar` <umangahirwar122@gmail.com>
2024-03-21 10:05:55 +0530

    second month
    internship second month programs

---
+++ Internship Training/javascriptfiles/forofloop.js
@@ -0,0 +1,15 @@
+let fruits = ["mango", "apple", "banana", "orange", "litchi"];
+for(fruit of fruits){
+    console.log(fruit);
+}
+for(char of "umang"){
+    console.log(char);
+}
+
+let heroes = [["superman", "batman", "wonder women"], ["spiderman", "ironman", "thor"]];
+for(list of heroes){
+    console.log(list);
+    for(hero of list){
+        console.log(hero);
+    }
+}
---
+++ Internship Training/javascriptfiles/nestedloopwithnestedarrays.js
@@ -0,0 +1,15 @@
+// let heroes = [['Iron man','Super man','Spider man'], ['Hulk','Batman','Ant man']];
+// console.log(heroes[0]);
+// console.log(heroes[1]);
+// for(let i = 0; i <= heroes.length-1; i++){
+//     for(let  j = 0; j <= heroes[i]; j++){
+//         console.log(j,heroes[i][j]);
+//     }
+// }
+
+let student = [ ["aman", 95], ["shraddha", 94.4], ["karan", 100] ];
+for(let i=0; i <= student.length-1; i++){
+    for(let j = 0; j <= student[i].length-1; j++){
+        console.log(student[i][j]);
+    }
+}
